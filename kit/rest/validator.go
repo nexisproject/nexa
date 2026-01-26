7bb51f3752abead7309369d30dc5b3b344cf8139
liasica <magicrolan@qq.com>
2026-01-27 06:32:13 +0800

    feat: 允许注册自定义校验方法

--- kit/rest/validator.go
+++ kit/rest/validator.go
@@ -13,7 +13,10 @@ import (
 
 type Validator struct {
 	validator *validator.Validate
+	trans     ut.Translator
 }
+
+type RegisterValidationFunc func(fn validator.Func) (err error)
 
 func NewValidator() *Validator {
 	zh := zhLocale.New()
@@ -24,7 +27,7 @@ func NewValidator() *Validator {
 
 	_ = zhTranslation.RegisterDefaultTranslations(validate, trans)
 
-	return &Validator{validator: validate}
+	return &Validator{validator: validate, trans: trans}
 }
 
 func (v *Validator) Validate(i any) error {
@@ -33,4 +36,34 @@ func (v *Validator) Validate(i any) error {
 	// }
 	// return nil
 	return v.validator.Struct(i)
+}
+
+// Validator 获取底层 validator 实例
+func (v *Validator) Validator() *validator.Validate {
+	return v.validator
+}
+
+// RegisterValidation 注册自定义校验方法
+func (v *Validator) RegisterValidation(tag string, message ...string) RegisterValidationFunc {
+	return func(fn validator.Func) (err error) {
+		err = v.validator.RegisterValidation(tag, fn)
+		if err != nil {
+			return err
+		}
+
+		return v.validator.RegisterTranslation(
+			tag,
+			v.trans,
+			func(ut ut.Translator) error {
+				text := "{0}验证失败"
+				if len(message) > 0 {
+					text = message[0]
+				}
+				return ut.Add(tag, text, true)
+			}, func(ut ut.Translator, fe validator.FieldError) string {
+				t, _ := ut.T(tag, fe.Field())
+				return t
+			},
+		)
+	}
 }
